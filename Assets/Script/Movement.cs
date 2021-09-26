6aded361262481fb401f393a6b6787c71093120b
Jiawei Liu <12638430@student.uts.edu.au>
2021-09-27 01:06:10 +1000

    90 percent complete

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //public float speed = 0.4f;
-    //Vector2 move = Vector2.zero;
+    public float speed = 1.0f;
+    private Vector2 direction = Vector2.zero;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        //move = transform.position;
+        //direction = transform.position;
     }
 
-    void FixedUpdate() {
-        //Vector2 go = Vector2.MoveTowards(transform.position, move, speed);
-        //GetComponent<Rigidbody2D>().MovePosition(go);
-
-        //if ((Vector2)transform.position == move){
-            //if (Input.GetKey(KeyCode.RightArrow) && effect(Vector2.right))
-               // move == (Vector2)transform.position + Vector2.right;
-
-            //if (Input.GetKey(KeyCode.LeftArrow) && effect(Vector2.left))
-              //  move == (Vector2)transform.position + Vector2.left;
-            
-            //if (Input.GetKey(KeyCode.UPArrow) && effect(Vector2.up))
-            //    move == (Vector2)transform.position + Vector2.up;
-            
-            //if (Input.GetKey(KeyCode.DownArrow) && effect(Vector2.down))
-              //  move == (Vector2)transform.position + Vector2.down;
-        //}
-    }
-
-    //bool effect(Vector2 dir){
-        //Vector2 address = transform.position;
-        //RaycastHit2D hit = Physics2D.Linecast(address + dir, address);
-        //return (hit.collider == GetComponent<collider2D>());
-    //}
-    // Update is called once per frame
     void Update()
     {
+        input();
+        move();
+    }
 
+    void input(){
+        
+        if (Input.GetKeyDown (KeyCode.RightArrow)){
+            direction = Vector2.right;
+
+        }else if (Input.GetKeyDown (KeyCode.LeftArrow)){
+            direction = Vector2.left;
+
+        }else if (Input.GetKeyDown (KeyCode.UpArrow)){
+            direction = Vector2.up;
+
+        }else if (Input.GetKeyDown (KeyCode.DownArrow)){
+            direction = Vector2.down;
+        }
+
+        //Animation connected
+       // Vector2 dir = direction - (Vector2)transform.position;
+       // GetComponent<Animator>().SetFloat("DirX", dir.x);
+        //GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+    void move(){
+        transform.localPosition += (Vector3)(direction * speed) * Time.deltaTime;
     }
 }
